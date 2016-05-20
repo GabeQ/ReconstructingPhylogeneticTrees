ecc2d689e739af313fb82278fa08105c2bbccc3a
chenpekker <cssummer16@c74.clinic.cs.hmc.edu>
2016-05-20 15:34:06 -0700

    added max Parsimony

--- Max Parismony Phlogenetic Trees.py
+++ Max Parismony Phlogenetic Trees.py
@@ -2,27 +2,23 @@
 
 
 def isLeaf(tree):
+	""" is the tree a leaf, returns true or false"""
 	if tree[1] == () and tree[2] == ():
 		return True
 	else:
 		return False
 
-
 def different(char, char1):
 	""" determines if we need to add a 1 or a 0 to the score of a letter """
-
 	if char == char1:
 		return 0
 	else:
 		return 1
 
-
 def bestSinglePosition(tree, character, position, tipMapping, memo):
-	"""memo is a dictionary that will be used for memoization"""
-
+	"""Returns the minimum parsimony score for a given position at a given nucleotide"""
 	if tree in memo:
 		return memo[tree]
-
 	else: 
 		if isLeaf(tree) == True:
 			char1 = tipMapping[tree[0]][position] # this would find the leaf in the dictionary, and the return the letter of the sequence of that leaf at the given position.
@@ -30,24 +26,37 @@ def bestSinglePosition(tree, character, position, tipMapping, memo):
 			if num1 == 1:
 				num1 = float("inf")
 			return num1
-
 		else:
 			root = tree[0]
 			leftTree = tree[1]
 			rightTree = tree[2]
-
 			miniL = float("inf")
 			for leftChar in ["A", "T", "C", "G"]:
 				ansL = bestSinglePosition(leftTree, leftChar, position, tipMapping, memo) + different(leftChar, character)
 				if ansL < miniL:
 					miniL = ansL
 			memo[leftTree[0]] = miniL
-
 			miniR = float("inf")
 			for rightChar in ["A", "T", "C", "G"]:
 				ansR = bestSinglePosition(rightTree, rightChar, position, tipMapping, memo) + different(rightChar, character)
 				if ansR < miniR:
 					miniR = ansR 
 			memo[rightTree[0]] = miniR
-
 			return miniR + miniL
+
+def maxParsimony(tree, tipMapping):
+	""" computes the best score for all positions in a sequence and for all possible characters"""
+	list1 = tipMapping.keys()
+	length = len(tipMapping[list1[0]])
+	ans = 0
+	for position in range(length):
+		minNum = float("inf")
+		for char in ["A", "T", "C", "G"]:
+			num1 = bestSinglePosition(tree, char, position, tipMapping, {})
+			if num1 < minNum:
+				minNum = num1
+		ans += minNum
+	return ans
+
+
+
