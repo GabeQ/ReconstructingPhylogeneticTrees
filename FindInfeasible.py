ee7d91a31ea565b2d0922eaca98c041655826848
mathagoris <matt.dohlen@gmail.com>
2016-06-01 10:50:47 -0700

    Updated FindInfeasible.py

--- FindInfeasible.py
+++ FindInfeasible.py
@@ -5,17 +5,16 @@ def getKey(item):
     return item.id
     
 def getGeneAndLocus(fileName):
-    if fileName.endswith('.align'):
-        myAlignment = AlignIO.read(fileName, "fasta")
-        geneLocusList = []
-        sorted(myAlignment, key=getKey)
-        for align in myAlignment:
-            geneLocusSpec = align.id.split('_')
-            geneLocus = (geneLocusSpec[0], geneLocusSpec[1])
-            if geneLocus not in geneLocusList:
-                geneLocusList.append(geneLocus)
-        return geneLocusList
-    return []
+    myAlignment = AlignIO.read(fileName, "fasta")
+    geneLocusList = []
+    myAlignment = sorted(myAlignment, key=getKey)
+    for align in myAlignment:
+        geneLocusSpec = align.id.split('_')
+        print geneLocusSpec
+        geneLocus = (geneLocusSpec[0], geneLocusSpec[1])
+        if geneLocus not in geneLocusList:
+            geneLocusList.append(geneLocus)
+    return geneLocusList
     
 
 def possiblyInfeasible(fileName):
@@ -26,17 +25,21 @@ def possiblyInfeasible(fileName):
             if geneLocusList[i][0] == geneLocusList[j][0] and geneLocusList[i][1] != geneLocusList[j][1]:
                 infeasibleGenes.union(geneLocusList[i][0])
                 break
-    return infeasibleGenes == set([])
+    if infeasibleGenes != set([]):
+        return fileName
+        
         
 
 def getPossiblyInfeasible(dataDirectory):
     potentialFiles = []
+    filePath = dataDirectory
     for dirName, subDirList, fileList in os.walk(dataDirectory):
-        if len(fileList) != 0:
-            for subDir in subDirList:
-                potentialFiles + getPossiblyInfeasible(subDir)
-        else:
-            for f in fileList:
-                if possiblyInfeasible(f):
-                    potentialFiles.append(f)
+        for subDir in subDirList:
+            subFilePath = filePath + "/" + subDir
+            for dirName, subDirList, fileList in os.walk(dataDirectory):
+                for subDir in subDirList:
+                    subSubFilePath = subFilePath + "/" + subDir
+                    for f in fileList:
+                        if f.endswith('.align') and possiblyInfeasible(subSubFilePath + "/" + f):
+                            potentialFiles.append(f)
     return potentialFiles
